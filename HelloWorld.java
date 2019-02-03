a3ea46bd408408e588f6f7f56f514b84372b3b0c
fimakoff <dm.efimenko@gmail.com>
2019-02-03 18:26:00 +0300

    Проставляем точки с запятыми

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 	Public static void main(String{} agrs){
-		System.out.println("Hello World!");
-		System.out.println("Hello everybody!");
-		System.out.println("Hello!");
+		System.out.println("Hello World!;");
+		System.out.println("Hello everybody!;");
+		System.out.println("Hello!;");
 	}
 }
