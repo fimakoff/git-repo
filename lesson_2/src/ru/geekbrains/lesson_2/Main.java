52f298559ef2fcd363e5d01afa91ed1216514ace
fimakoff <dm.efimenko@gmail.com>
2019-02-09 23:54:04 +0300

    Lesson 2

---
+++ lesson_2/src/ru/geekbrains/lesson_2/Main.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson_2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Введите число:");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println("a = " + a);
+    }
+}
