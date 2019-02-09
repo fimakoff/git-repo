566b4c75ffc68747026c008deab4f734efdd6f5b
fimakoff <dm.efimenko@gmail.com>
2019-02-10 00:29:18 +0300

    upgrade range

--- guess_the_number/src/ru/geekbrains/guess_the_number/Main.java
+++ guess_the_number/src/ru/geekbrains/guess_the_number/Main.java
@@ -8,18 +8,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ваша задача угадать число.");
         int range = 50;
+        int total = 1;
         int number = (int)(Math.random() * range);
         while(true){
             System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
             if (input_number == number){
                 System.out.println("Вы угадали!");
+
+                System.out.println("Количество попыток: " + total);
                 break;
             } else if (input_number > number){
                 System.out.println("Загаданное число меньше");
             } else{
                 System.out.println("Загаданное число больше");
             }
+            total++;
         }
         scanner.close();
     }
